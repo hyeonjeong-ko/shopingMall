8e5e8b1879bb416e3b4a496a1b1741626060d4ce
hyeonjeong-ko <hyeonkjjang@gmail.com>
2025-05-25 11:52:51 +0900

    🔐 feat: 현재 사용자 인증 어노테이션 추가 및 시큐리티 설정 개선
    - @CurrentUser 어노테이션 기반 인증 유틸리티 추가
    - SecurityConfig에서 인증 정보 주입 설정 적용

--- src/main/java/goorm/server/timedeal/config/SecurityConfig.java
+++ src/main/java/goorm/server/timedeal/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
